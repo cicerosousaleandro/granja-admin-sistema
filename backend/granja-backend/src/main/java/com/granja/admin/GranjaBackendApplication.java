44f0f7d9309bc3510039a1e4cd9c3e7e7260e9cd
Cícero Sousa <cicerosousa.le@gmail.com>
2026-03-24 19:09:09 -0300

    feat: projeto Spring Boot criado - estrutura inicial configurada

---
+++ backend/granja-backend/src/main/java/com/granja/admin/GranjaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.granja.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranjaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GranjaBackendApplication.class, args);
+    }
+
+}
---
+++ backend/granja-backend/src/test/java/com/granja/admin/GranjaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.granja.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranjaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
